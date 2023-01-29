98667118d106e351e257268d996df6a0a7332da6
JuliaLC1 <julialussiercraig@gmail.com>
2023-01-29 18:07:06 +0000

    added 5 day forecast

--- src/TemperatureUnites.js
+++ src/TemperatureUnites.js
@@ -1,44 +1,10 @@
 import React, { useState } from "react";
 
 export default function TemperatureUnits(props) {
-  const [unit, setUnit] = useState("celsius");
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  function fahrenheit() {
-    return (props.celsius * 9) / 5 + 32;
-  }
-  if (unit === "celsius") {
-    return (
-      <div className="temp-info">
-        <strong id="temp-number">{Math.round(props.celsius)}</strong>
-        <span className="units">
-          °C |{" "}
-          <a href="/" onClick={showFahrenheit}>
-            {" "}
-            °F
-          </a>
-        </span>
-      </div>
-    );
-  } else {
-    return (
-      <div className="temp-info">
-        <strong id="temp-number">{Math.round(fahrenheit())}</strong>
-        <span className="units">
-          <a href="/" onClick={showCelsius}>
-            °C{" "}
-          </a>
-          | °F
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div className="temp-info">
+      <strong id="temp-number">{Math.round(props.celsius)}</strong>
+      <span className="units">°C</span>
+    </div>
+  );
 }
